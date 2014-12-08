df75c4c7e29a86a1f5313380963ed710b7abc442
Logan Short <lshort@stanford.edu>
2014-12-08 00:53:33 -0800

    Updated naive bayes to work with cv and comtagcombine

--- nbtext.py
+++ nbtext.py
@@ -5,60 +5,99 @@ from sklearn.naive_bayes import MultinomialNB as mnb
 import loadData
 import wordvectors
 
-def recallTest(numQuestions, clf, k):
-  recallSum = 0
-  for i in range(0, numQuestions):
-    tagProb = clf.predict_proba(X[i])   
+# Get top tags. Diction of <int, int>: tagID to count in first 50,000 questions
+# Only includes tags which appeared more than 50 times.
+tcount_infile = open('tcount-50000.txt', 'r')
+tempTopTags = json.load(tcount_infile)
+tcount_infile.close()
+topTags = {}
+for idStr, count in tempTopTags.items():
+  topTags[int(idStr)] = count
 
-def multinomialNaiveBayesTrain(numQuestions, tag, X, y):
+def normalizeProb(tagProbD):
+  maxProb = tagProbD[max(tagProbD, key=lambda x: tagProbD[x])]
+  minProb = tagProbD[min(tagProbD, key=lambda x: tagProbD[x])]
+  for tag in tagProbD:
+    tagProbD[tag] = (tagProbD[tag] - minProb)/(maxProb - minProb)
+
+def recall_k(k, topTags, actualTags):
+  recall_score = 0.0
+  for i in xrange(k):
+    if topTags[i] in actualTags:
+      recall_score += 1.0
+  return recall_score / len(actualTags)
+
+def multinomialNaiveBayesTrain(trainQuestions, tag, X, y, mnbd):
   clf = mnb()
   i = 0
-  for qid in loadData.questions:
-    if tag in loadData.questions[qid].tags:
+  for qid in trainQuestions:
+    if tag in trainQuestions[qid].tags:
       y[i] = 1
     else:
       y[i] = 0
     i += 1
 
   clf.fit(X,y)
-  print "Fit Complete"
+  mnbd[tag] = clf
+
+"""
+Trains multinomial naive bayes models for each tag and stores them
+in a dict that is returned
+"""
+def getTagNaiveBayesScores(trainQuestions, topTags, wordToIndex, wordVecs):
+  mnbd = {}
+  X = np.zeros((len(trainQuestions), len(wordToIndex)))
+  y = np.zeros(len(trainQuestions))
   i = 0
-  tagProb = clf.predict_proba(X)
-  for qid in loadData.questions:
-    probList = recallProbs[qid]    
-    probList.append((tag, tagProb[i][1]))
-    sortedProbs = sorted(probList, key=operator.itemgetter(1), reverse=True)
-    sortedProbs.pop()
-    recallProbs[qid] = sortedProbs
+  for qid in trainQuestions:
+    X[i] = np.array(wordVecs[qid])
     i += 1
+  for t in topTags:
+    multinomialNaiveBayesTrain(trainQuestions, t, X, y, mnbd)
+  return mnbd
 
-y = np.zeros(loadData.NUM_QUESTIONS)
-X = np.zeros((loadData.NUM_QUESTIONS,len(wordToIndex)))
-i = 0
-for qid in loadData.questions:
-  X[i] = np.array(wordVecs[qid])
-  i += 1
+"""
+Returns a normalized probability dictionary of relevant tags for
+an input question
+"""
+def getProbForQuestion(qid, mnbd, topTags, wordVecs):
+  questionNBD = {}
+  for tag in topTags:
+    clf = mnbd[tag]
+    X = np.array(wordVecs[qid])
+    tagProb = clf.predict_proba(X)
+    questionNBD[tag] = tagProb[0][1]
+  normalizeProb(questionNBD)
+  return questionNBD  
 
-recallProbs = {}
-for qid in loadData.questions:
-  recallProbs[qid] = [(-1,-1), (-1,-1), (-1,-1), (-1,-1), (-1,-1), (-1,-1), (-1,-1), (-1,-1), (-1,-1), (-1,-1)]
+def modelNaiveBayes(testQuestions, mnbd, topTags, wordVecs):
+  numTest = len(testQuestions)
+  recall_5_sum = 0.0
+  recall_10_sum = 0.0
+  i = 0
+  for (qid, question) in testQuestions.items():
+    if i % 1000 == 0:
+      print i
+    tagD = getProbForQuestion(qid, mnbd, topTags, wordVecs)
+    sortedTags = sorted(tagD, key=lambda x: tagD[x], reverse=True)
+    num = min(10, len(sortedTags))
+    bestTags = sortedTags[0:num]
+    recall_5 = recall_k(5, bestTags, question.tags)
+    recall_10 = recall_k(10, bestTags, question.tags)
+    recall_5_sum += recall_5
+    recall_10_sum += recall_10
+    i += 1
+  recall_5_avg = recall_5_sum / numTest
+  recall_10_avg = recall_10_sum / numTest
+  return recall_5_avg, recall_10_avg
 
-tcount = open('tcount-50000.txt')
-tagsf = json.load(tcount)
-
-i = 0
-for tag in tagsf:
-  print loadData.tags[int(tag)].tag
-  multinomialNaiveBayesTrain(loadData.NUM_QUESTIONS, int(tag), X, y)
-
-recallSum = 0
-for qid in loadData.questions:
-  recallValue = 0
-  questionTags = loadData.questions[qid].tags
-  for recallTag in recallProbs[qid]:
-    if recallTag[0] in questionTags:
-      recallValue += 1
-  recallSum += (recallValue*1.0)/len(questionTags)
-print (recallSum*1.0)/loadData.NUM_QUESTIONS
-
-
+for fold in folds:
+  trainQuestions = fold[0]
+  mnbd = getTagNaiveBayesScores(trainQuestions, topTags, wordToIndex, wordVecs)
+  print "Training Naive Bayes Completed"
+  testQuestions = fold[1]
+  recall_5, recall_10 = modelNaiveBayes(testQuestions, mnbd, topTags, wordVecs)
+  print recall_5
+  print recall_10
+  
+  
